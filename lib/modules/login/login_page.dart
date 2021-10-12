76f3c9a1f33ac6271b48904cfc796e082dad9ece
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-12 00:56:53 -0300

    feat: google signin

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -56,9 +57,21 @@ class _LoginPageState extends State<LoginPage> {
                   Padding(
                     padding:
                         const EdgeInsets.symmetric(horizontal: 40, vertical: 0),
-                    child: SocialLoginButton(onTap: () {
-                      print("pressionou!");
-                    }),
+                    child: SocialLoginButton(
+                      onTap: () async {
+                        GoogleSignIn _googleSignIn = GoogleSignIn(
+                          scopes: [
+                            'email',
+                          ],
+                        );
+                        try {
+                          final response = await _googleSignIn.signIn();
+                          print(response);
+                        } catch (error) {
+                          print(error);
+                        }
+                      },
+                    ),
                   ),
                 ],
               ),
