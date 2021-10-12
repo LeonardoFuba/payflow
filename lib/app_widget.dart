6c139e96cc041960d345536706a835a1c75276eb
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-12 17:02:43 -0300

    dev: named routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'modules/splash/splash_page.dart';
 import 'modules/login/login_page.dart';
+import 'modules/home/home_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,10 +12,13 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
+      theme: ThemeData(primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
