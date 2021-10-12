48060ab99db44653f9701477f9f0c5acfbf1e1e1
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-11 23:28:40 -0300

    dev: create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.40,
+              color: AppColors.primary,
+            ),
+            Positioned(
+              top: size.height * 0.07,
+              left: 0,
+              right: 0,
+              child: Image.asset(AppImages.person, height: size.height * 0.50),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: size.height * 0.57,
+                bottom: 16,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Text(
+                      "Organize seus\nboletos em um\nsó lugar",
+                      textAlign: TextAlign.center,
+                      style: AppTextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 40, vertical: 0),
+                    child: SocialLoginButton(onTap: () {
+                      print("pressionou!");
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
