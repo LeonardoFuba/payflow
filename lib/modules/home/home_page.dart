2a6b7ea71af4aa33c0e1b703314bca4ab5e10ca2
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-15 21:16:32 -0300

    dev: home navigation

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,13 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = HomeController();
+  final pages = [
+    Container(child: Center(child: Text("boletos"))),
+    Container(child: Center(child: Text("extratos"))),
+    Container(child: Center(child: Text("scanner"))),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(128),
+        child: Container(
+          height: 128,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                        text: "Leonardo",
+                        style: AppTextStyles.titleBoldBackground),
+                  ],
+                ),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: AppTextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                homeController.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(Icons.home),
+              color: AppColors.primary,
+            ),
+            Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                onPressed: () {
+                  homeController.setPage(2);
+                  setState(() {});
+                },
+                icon: Icon(Icons.add_box_outlined),
+                color: AppColors.background,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                homeController.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(Icons.article_outlined),
+              color: AppColors.body,
+            ),
+          ],
+        ),
       ),
     );
   }
